2fcb230df7f187b9c0e402cdf23254461f801481
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-05-01 12:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function() {
-  // Create a new p element with the text "DOM load success"
-  var p = document.createElement("p");
-  var text = document.createTextNode("DOM load success");
-  p.appendChild(text);
-
-  // Add the new p element to the body of the HTML document
-  document.body.appendChild(p);
-});
+window.onload = (event) => {
+document.write('DOM load success');
+}
